intel/mainline-tracking

i3c: mipi-i3c-hci-pci: Add support for Intel Lakefield I3C
Add I3C controller PCI ID on Intel Lakefield.

Signed-off-by: Jarkko Nikula <jarkko.nikula@linux.intel.com>

drivers/i3c/master/mipi-i3c-hci/mipi-i3c-hci-pci.c
@@ -126,6 +126,8 @@ static void mipi_i3c_hci_pci_remove(struct pci_dev *pci)
 static const struct pci_device_id mipi_i3c_hci_pci_devices[] = {
 	/* Meteor Lake-P */
 	{ PCI_VDEVICE(INTEL, 0x7e7c), (kernel_ulong_t)&intel_info},
+	/* Lakefield */
+	{ PCI_VDEVICE(INTEL, 0x98ec), (kernel_ulong_t)&intel_info},
 	{ },
 };
 MODULE_DEVICE_TABLE(pci, mipi_i3c_hci_pci_devices);

ahunter6/linux

drivers/i3c/master/mipi-i3c-hci/mipi-i3c-hci-pci.c
@@ -479,6 +479,8 @@ static const struct pci_device_id mipi_i3c_hci_pci_devices[] = {
 	{ PCI_VDEVICE(INTEL, 0x4d6f), (kernel_ulong_t)&intel_si_2_info},
 	/* Meteor Lake-P */
 	{ PCI_VDEVICE(INTEL, 0x7e7c), (kernel_ulong_t)&intel_si_1_info},
+	/* Lakefield */
+	{ PCI_VDEVICE(INTEL, 0x98ec), (kernel_ulong_t)&intel_si_1_info},
 	/* Panther Lake-H */
 	{ PCI_VDEVICE(INTEL, 0xe37c), (kernel_ulong_t)&intel_mi_1_info},
 	{ PCI_VDEVICE(INTEL, 0xe36f), (kernel_ulong_t)&intel_si_2_info},